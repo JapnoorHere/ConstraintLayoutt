ad78b0708680cb5c348aff5bbaa4331727d5ded4
Japnoor2104 <sjapnoor11@gmail.com>
2022-07-30 08:04:55 +0530

    change

--- app/src/main/java/com/japnoor/constraintlayoutt/MainActivity.kt
+++ app/src/main/java/com/japnoor/constraintlayoutt/MainActivity.kt
@@ -2,10 +2,16 @@ package com.japnoor.constraintlayoutt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.japnoor.constraintlayoutt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
     }
 }
